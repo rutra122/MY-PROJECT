5c6b349c0260c762099cb18598a61c621081a41b
henrique lima <henriquelima1408@hotmail.com>
2021-09-12 16:43:37 -0300

    Vai

--- MY PROJECT/Assets/MYPROJECT.cs
+++ MY PROJECT/Assets/MYPROJECT.cs
@@ -1,33 +1,111 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MYPROJECT : MonoBehaviour
 {
-    public InputField LOGIN;
-    public InputField SENHA;
-    public Button FAZERLOGIN;
-    public Text RESPOSTALOGIN;
+    [SerializeField]
+    private InputField loginField;
+    [SerializeField]
+    private InputField passwordField;
+    [SerializeField]
+    private Button loginButton;
+    [SerializeField]
+    private Text reponseText;
+
+    [SerializeField]
+    private int test_userAmount;
+
+    [SerializeField]
+    private EXCEL excel = new EXCEL();
 
     // Start is called before the first frame update
     void Start()
     {
-        FAZERLOGIN.onClick.AddListener(() => { CADASTRO(); });
+        for (int i = 0; i < test_userAmount; i++)
+        {
+            excel.AddUser(i.ToString(), "");
+        }
+
+        loginButton.onClick.AddListener(() =>
+        {
+            if (Login(loginField.text, passwordField.text))
+            {
+
+                reponseText.text = "Successful login";
+            }
+            else
+            {
+
+                reponseText.text = "Senha ou login incorreto";
+                Cadastro(loginField.text, passwordField.text);
+            }
+        });
     }
 
-    // Update is called once per frame
-    void CADASTRO()
+    bool Login(string userID, string password)
     {
-        if (LOGIN == LOGIN DO BANCO DE DADOS)
+        return excel.IsUserValid(userID, password);
+    }
+
+    void Cadastro(string userID, string password)
+    {
+        excel.AddUser(userID, password);
+    }
+}
+
+[System.Serializable]
+public class User
+{
+
+    public string UserName;
+    public string PassWord;
+
+    public User(string userName, string passWord)
+    {
+        UserName = userName;
+        PassWord = passWord;
+    }
+}
+
+[System.Serializable]
+public class EXCEL
+{
+    [SerializeField]
+    private List<User> users = new List<User>();
+
+    public bool IsUserValid(string userName, string password)
+    {
+
+        for (int i = 0; i < users.Count; i++)
+        {
+            if (users[i].UserName == userName && users[i].PassWord == password)
             {
-            if (SENHA == SENHA DO MESMO LOGIN)
-                {//ACESSO CONCLUIDO AO APP
+
+                return true;
             }
-        else
-            { RESPOSTALOGIN.text = "Senha ou login incorreto"; }
         }
-        else
-        { RESPOSTALOGIN.text = "Senha ou login incorreto"; }
+
+        return false;
+    }
+
+    public bool UserExist(string userName)
+    {
+        for (int i = 0; i < users.Count; i++)
+        {
+            if (users[i].UserName == userName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void AddUser(string userName, string password)
+    {
+        if (!UserExist(userName))
+        {
+            users.Add(new User(userName, password));
+        }
     }
 }
