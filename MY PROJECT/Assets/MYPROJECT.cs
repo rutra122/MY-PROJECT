f1034566997c93605486cbb09114bcef4e9b2dc9
Artur <arturinocencioo@gmail.com>
2021-09-12 15:52:00 -0300

    123123

--- MY PROJECT/Assets/MYPROJECT.cs
+++ MY PROJECT/Assets/MYPROJECT.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MYPROJECT : MonoBehaviour
 {
+    public InputField LOGIN;
+    public InputField SENHA;
+    public Button FAZERLOGIN;
+    public Text RESPOSTALOGIN;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        FAZERLOGIN.onClick.AddListener(() => { CADASTRO(); });
     }
 
     // Update is called once per frame
-    void Update()
+    void CADASTRO()
     {
-        
+        if (LOGIN == LOGIN DO BANCO DE DADOS)
+            {
+            if (SENHA == SENHA DO MESMO LOGIN)
+                {//ACESSO CONCLUIDO AO APP
+            }
+        else
+            { RESPOSTALOGIN.text = "Senha ou login incorreto"; }
+        }
+        else
+        { RESPOSTALOGIN.text = "Senha ou login incorreto"; }
     }
 }
